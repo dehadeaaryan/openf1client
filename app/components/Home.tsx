c473f00e540ef5994867a217bf76a4e263cfc1d6
dehadeaaryan <dehadeaaryan@gmail.com>
2024-06-22 02:31:21 -0700

    fixed character error

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     const [loading, setLoading] = useState(false);
     return (
         <div className="p-10 bg-background w-screen h-full flex flex-col justify-center items-center gap-10 box-border">
-            <p className="text-primary">I'm still building this. Check out <Link href="/current" className="underline">the website</Link> or talk to my AI assistant:</p>
+            <p className="text-primary">I&apos;m still building this. Check out <Link href="/current" className="underline">the website</Link> or talk to my AI assistant:</p>
             <Output output={output} loading={loading} />
             <Form setOutput={setOutput} loading={loading} setLoading={setLoading} />
         </div>

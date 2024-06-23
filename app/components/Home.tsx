3264da1560058ba4c29af336d47b9a00effae405
dehadeaaryan <dehadeaaryan@gmail.com>
2024-06-23 00:02:40 -0700

    fix

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -9,6 +9,9 @@ export default function Home() {
     const [prompt, setPrompt] = useState<string[]>([]);
     const [output, setOutput] = useState<string[]>([]);
     const [loading, setLoading] = useState(false);
+    if ('virtualKeyboard' in navigator) {
+        (navigator as any).virtualKeyboard.overlaysContent = true;
+    }
     return (
         <div className="p-10 pb-4 bg-background w-full h-full flex flex-col justify-center items-center gap-10 box-border">
             <p className="text-primary">I&apos;m still building this. Check out <Link href="/current" className="underline">the website</Link> or talk to my AI assistant:</p>
--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,6 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    if ('virtualKeyboard' in navigator) {
-        (navigator as any).virtualKeyboard.overlaysContent = true;
-    }
     return (
         <html lang="en">
             <body className={inter.className}>{children}</body>
